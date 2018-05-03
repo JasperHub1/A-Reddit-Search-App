e724eef496cc2771780ba5649054a98f3a528e7e
JasperHub1 <31580595+JasperHub1@users.noreply.github.com>
2018-05-03 22:30:34 +1000

    initial commit

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ function truncateText(text, limit) {
 
 // Search
 function search(searchTerm, searchLimit, sortBy) {
-  return fetch(`http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
+  return fetch(`https://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
   .then(res => res.json())
   .then(data => data.data.children.map(data => data.data))
   .catch(err => console.log(err));
